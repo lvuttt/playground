0eec050585ca56df24e80e18858e6ff0a13fe483
nuttawat.y <nuttawat.y@kbtg.tech>
2020-05-29 23:50:51 +0700

    finish all database function

--- assignment2/db/model.js
+++ assignment2/db/model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-// const { } = require('../helpers')
+const { CustomError } = require('../error')
 
 class DatabaseManager {
     constructor(json_path) {
@@ -20,60 +20,61 @@ class DatabaseManager {
         fs.writeFile(this.json_path, jsonContent, 'utf8', function (err) {
             if (err) {
                 console.log("An error occured while writing JSON Object to File.");
-                throw (err)
+                throw new CustomError(500, err)
             }
-            console.log("JSON file has been saved.");
+            console.log("JSON file has been updated.");
         });
     }
 
     countGroup() {
-        try {
-            var counter = {}
+        var counter = {}
 
-            // Empty group has 0 contact. So fill this first
-            var emptyNameRow = this.dictionary.filter((row) => { return !row.firstName })
-            var emptyGroupName = emptyNameRow.map(function (row) { return row.groupName })
-            emptyGroupName.forEach(function (name) { counter[name] = 0; })
+        // Empty group has 0 contact. So initiate every groups with 0 contact.
+        var uniqueGroupNames = this.getUniqueGroupNames()
+        uniqueGroupNames.forEach(function (name) { counter[name] = 0; })
 
-            // Fill the rest of group
-            var data = this.dictionary.filter((row) => { return row.firstName })
-            var groupName = data.map(function (row) { return row.groupName })
-            groupName.forEach(function (name) { if (name in counter) counter[name]++; else counter[name] = 1; });
+        // Count the rest of group.
+        var filteredData = this.dictionary.filter((row) => { return row.firstName })
+        var groupNames = filteredData.map(function (row) { return row.groupName })
+        groupNames.forEach(function (name) { counter[name]++ });
 
-            return {
-                "groups": counter
-            }
-        } catch (err) {
-            throw (err)
+        return {
+            "groups": counter
         }
     }
 
     deleteGroup(groupName) {
-        try {
-            this.dictionary = this.dictionary.filter((row) => { return row.groupName != groupName })
-            this.write()
-            return this.dictionary
-        } catch (err) {
-            throw (err)
+        var uniqueGroupNames = this.getUniqueGroupNames()
+        if (!uniqueGroupNames.includes(groupName)) {
+            throw new CustomError(404, "Group name not exists")
+        }
+
+        this.dictionary = this.dictionary.filter((row) => { return row.groupName != groupName })
+        this.write()
+        return {
+            "message": `Group(${groupName}) is deleted.`
         }
     }
 
     addGroup(groupName) {
-        try {
-            var data = {
-                "groupName": groupName,
-                "firstName": "",
-                "birthDate": "",
-                "phoneNum": "",
-                "email": "",
-                "url": "",
-                "imagePath": ""
-            }
-            this.dictionary.push(data)
-            this.write()
-            return this.dictionary
-        } catch (err) {
-            throw (err)
+        var uniqueGroupNames = this.getUniqueGroupNames()
+        if (uniqueGroupNames.includes(groupName)) {
+            throw new CustomError(404, "Group name already exists")
+        }
+
+        var data = {
+            "groupName": groupName,
+            "firstName": "",
+            "birthDate": "",
+            "phoneNum": "",
+            "email": "",
+            "url": "",
+            "imagePath": ""
+        }
+        this.dictionary.push(data)
+        this.write()
+        return {
+            "message": `Group(${groupName}) is created.`
         }
     }
 
@@ -81,7 +82,12 @@ class DatabaseManager {
         try {
 
             if (!groupName) {
-                throw (Error("Group name must not be empty"))
+                throw new CustomError(400, "Group name must not be empty.")
+            }
+
+            var uniqueGroupNames = this.getUniqueGroupNames()
+            if (!uniqueGroupNames.includes(groupName)) {
+                throw new CustomError(404, "Group name not exists.")
             }
 
             var contactList = this.dictionary.filter((row) => { return row.groupName == groupName && row.firstName }).map((row) => { return row.firstName })
@@ -90,51 +96,130 @@ class DatabaseManager {
             }
 
         } catch (err) {
-            throw (err)
+            throw new CustomError(500, err)
         }
     }
 
     addContact(info = {}) {
-        try {
-            var groupNames = this.dictionary.filter((row) => { return !row.firstName }).map((row) => { return row.groupName })
-            if (!groupNames.includes(info.groupName || "")) {
-                throw (Error("Group must be created first"))
-            }
+        var uniqueGroupNames = this.getUniqueGroupNames()
+        if (!uniqueGroupNames.includes(info.groupName || "")) {
+            throw new CustomError(400, "Group must be created first")
+        }
 
-            if ((info.firstName || "") == "") {
-                throw (Error("First Name must not be empty"))
-            }
+        if ((info.groupName || "") == "") {
+            throw new CustomError(400, "Group name must not be empty")
+        }
 
-            this.dictionary.push({
-                groupName: groupName,
-                firstName: info.firstName,
-                birthDate: info.birthDate || "",
-                phoneNum: info.phoneNum || "",
-                email: info.email || "",
-                url: info.url || "",
-                imagePath: info.imagePath || ""
-            })
-            this.write()
+        if ((info.firstName || "") == "") {
+            throw new CustomError(400, "First name must not be empty")
+        }
 
-        } catch (err) {
-            throw (err)
+        var filteredData = this.dictionary.filter((row) => { return row.groupName == info.groupName && row.firstName == info.firstName })
+        if (filteredData.length != 0) {
+            throw new CustomError(400, "First name already exists")
+        }
+
+
+        this.dictionary.push({
+            groupName: info.groupName,
+            firstName: info.firstName,
+            birthDate: info.birthDate || "",
+            phoneNum: info.phoneNum || "",
+            email: info.email || "",
+            url: info.url || "",
+            imagePath: info.imagePath || ""
+        })
+        this.write()
+        return {
+            "message": `Contact(${info.firstName}) is added.`
         }
     }
 
-    deletContact(groupName, name) {
+    deleteContact(groupName, name) {
+        var uniqueGroupNames = this.getUniqueGroupNames()
+        if (!uniqueGroupNames.includes(groupName)) {
+            throw new CustomError(404, "Group name not exists.")
+        }
 
+        if ((name || "") == "") {
+            throw new CustomError(400, "First name must not be empty")
+        }
+
+        var filteredData = this.dictionary.filter((row) => { return row.groupName == groupName && row.firstName == name })
+        if (filteredData.length == 0) {
+            throw new CustomError(404, "First name not exists")
+        }
+
+        this.dictionary = this.dictionary.filter((row) => { return row.groupName != groupName && row.firstName != name })
+        this.write()
+        return {
+            "message": `Contact(${info.firstName}) is deleted.`
+        }
     }
 
-    editContact(info={}) {
+    editContact(info = {}) {
+        var uniqueGroupNames = this.getUniqueGroupNames()
+        if (!uniqueGroupNames.includes((info.groupName || ""))) {
+            throw new CustomError(404, "Group name not exists.")
+        }
 
+        if ((info.firstName || "") == "") {
+            throw new CustomError(400, "First name must not be empty")
+        }
+
+        var filteredData = this.dictionary.filter((row) => { return row.groupName == info.groupName && row.firstName == info.firstName })
+        if (filteredData.length == 0) {
+            throw new CustomError(404, "First name not exists")
+        }
+
+        if ((info.newFirstName || "") == "") {
+            throw new CustomError(400, "Edited first name must not be empty")
+        }
+
+        var names = this.getContactList(info.groupName).contacts
+        if (names.includes(info.newFirstName)) {
+            throw new CustomError(400, "New first name already exists.")
+        }
+
+        this.dictionary.filter((row) => {
+            if (row.groupName == info.groupName && row.firstName == info.firstName) {
+                row.firstName = info.newFirstName
+                row.birthDate = info.birthDate || ""
+                row.phoneNum = info.phoneNum || ""
+                row.email = info.email || ""
+                row.url = info.url || ""
+                row.imagePath = info.imagePath || ""
+            }
+        })
+        
+        this.write()
+        return { "message": `Contact is edited.` }
     }
+
 
     getContactInfo(groupName, name) {
-        
+        var uniqueGroupNames = this.getUniqueGroupNames()
+        if (!uniqueGroupNames.includes((groupName || ""))) {
+            throw new CustomError(400, "Group name not exists.")
+        }
+
+        if ((name || "") == "") {
+            throw new CustomError(400, "First name must not be empty")
+        }
+
+        var filteredData = this.dictionary.filter((row) => { return row.groupName == groupName && row.firstName == name })
+        if (filteredData.length == 0) {
+            throw new CustomError(400, "First name not exists")
+        }
+        if (filteredData.length >= 2) {
+            throw new CustomError(500, "Data is duplicated")
+        }
+        return filteredData[0]
     }
 
-
-
+    getUniqueGroupNames() {
+        return this.dictionary.filter((row) => { return !row.firstName }).map(function (row) { return row.groupName })
+    }
 
 }
 
