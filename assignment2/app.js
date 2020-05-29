8ea8ad3e547fe9915d05f7ebad6ef1772206529c
nuttawat.y <nuttawat.y@kbtg.tech>
2020-05-29 17:04:00 +0700

    structure the files

--- assignment2/app.js
+++ assignment2/app.js
@@ -8,25 +8,6 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/mail/send', function (req, res) {
-  body = req.body
-  sendEmail({
-    "to": body.to,
-    "from": body.from,
-    "subject": "Send Grid",
-    "text": body.message,
-  }).then(() => {
-    return res.json({
-      "message": "Email has been sent"
-    });
-  }).catch((error) => {
-    res.status(error.code)
-    return res.json(error.response.body);
-  })
-});
-
-
-
 app.listen(port, () => {
   console.log(`Assignment2 app listening at http://localhost:${port}`)
 })
